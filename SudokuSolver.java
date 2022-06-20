4c23b8693de6fec9a1beedff455daab19ddc0951
Daniel Danio <102267797+xXDD01Xx@users.noreply.github.com>
2022-06-20 13:56:06 -0400

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,145 @@
+/*
+1_Define Grid, Insert Puzzle
+2_Row Method
+3_Column Method
+4_3x3 Box Method
+5_Combination Check Method For Above
+6_Solve Method w/ Recursive Call
+
+ */
+
+
+public class SudokuSolver
+{
+    private static final int GRID_SIZE = 9;     //const
+
+    public static void main(String[] args)
+    {
+        int[][] board =
+            {
+                {7, 0, 2, 0, 5, 0, 6, 0, 0},
+                {0, 0, 0, 0, 0, 3, 0, 0, 0},
+                {1, 0, 0, 0, 0, 9, 5, 0, 0},
+                {8, 0, 0, 0, 0, 0, 0, 9, 0},
+                {0, 4, 3, 0, 0, 0, 7, 5, 0},
+                {0, 9, 0, 0, 0, 0, 0, 0, 8},
+                {0, 0, 9, 7, 0, 0, 0, 0, 5},
+                {0, 0, 0, 2, 0, 0, 0, 0, 0},
+                {0, 0, 7, 0, 4, 0, 2, 0, 3}
+            };
+
+        printBoard(board);
+
+        if(solveBoard(board))
+        {
+            System.out.println("Solved Successfully!");
+        }
+        else
+        {
+            System.out.println("Invalid Board!");
+        }
+
+        printBoard(board);
+    }
+
+    private static void printBoard(int[][] board)
+    {
+        for(int row = 0; row < GRID_SIZE; row++)
+        {
+            if(row % 3 ==0 && row != 0)
+            {
+                System.out.println("-----------");
+            }
+            for(int column = 0; column < GRID_SIZE; column++)
+            {
+                if(column % 3 == 0 && column != 0)
+                {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static boolean isNumberInRow(int[][] board, int numberPreset, int row)
+    {
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            if (board[row][i] == numberPreset)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInColumn(int[][] board, int numberPreset, int column)
+    {
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            if (board[i][column] == numberPreset)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberIn3x3Box(int[][] board, int numberPreset, int row, int column)
+    {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+
+        for(int i = localBoxRow; i < localBoxRow + 3; i++)
+        {
+            for(int j = localBoxColumn; j < localBoxColumn + 3; j++)
+            {
+                if(board[i][j] == numberPreset)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int numberPreset, int row, int column)
+    {
+        return !isNumberInRow(board, numberPreset, row) &&
+                !isNumberInColumn(board, numberPreset, column) &&
+                !isNumberIn3x3Box(board, numberPreset, row, column);
+    }
+
+    private static boolean solveBoard(int[][] board)
+    {
+        for(int row = 0; row < GRID_SIZE; row++)
+        {
+            for(int column = 0; column < GRID_SIZE; column++)
+            {
+                if(board[row][column] == 0)
+                {
+                    for(int numberGuess = 1; numberGuess <= GRID_SIZE; numberGuess++)
+                    {
+                        if (isValidPlacement(board, numberGuess, row, column))
+                        {
+                            board[row][column] = numberGuess;
+
+                              if(solveBoard(board))
+                              {
+                                  return true;
+                              }
+                              else
+                              {
+                                  board[row][column] = 0;
+                              }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
